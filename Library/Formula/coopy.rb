014225f0d7bcd3dc3a845ccdf720a0f9b5374983
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2012-02-19 22:24:29 +0100

    it is easier to build coopy now that gnumeric is in brew

---
+++ Library/Formula/coopy.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class Coopy < Formula
+  url 'https://github.com/paulfitz/coopy/tarball/v0.5.5'
+  homepage 'http://share.find.coop'
+  head 'git://github.com/paulfitz/coopy.git'
+
+  depends_on 'cmake' => :build
+
+  depends_on 'gnumeric' unless ARGV.include? "--no-gnumeric"
+  depends_on 'mysql-connector-c' unless ARGV.include? "--no-mysql"
+  depends_on 'mdbtools' unless ARGV.include? "--no-mdb"
+  # depends_on 'gettext'
+  depends_on 'wxmac' if ARGV.include? "--wx"
+
+  def options
+    [
+      ["--no-gnumeric", "Build without spreadsheet support."],
+      ["--no-mysql", "Build without MYSQL support."],
+      ["--no-access", "Build without MDB (MS Access) support."],
+      ["--wx", "Build with wxWidgets support, for GUI."],
+    ]
+  end
+
+  def install
+    parameters = std_cmake_parameters
+    parameters << " -DUSE_GNUMERIC=TRUE" unless ARGV.include? "--no-gnumeric"
+    parameters << " -DUSE_REMOTE_SQL=TRUE -DUSE_MYSQL=TRUE" unless ARGV.include? "--no-mysql"
+    parameters << " -DUSE_ACCESS=TRUE" unless ARGV.include? "--no-access"
+    parameters << " -DCOMPILE_GUI=FALSE" unless ARGV.include? "--wx"
+    system "cmake #{parameters}"
+    system "make install"
+  end
+end
